3b2783c39fe2c0ce60cdf741c96e5d15ef9d8311
bloodyaxe <github@marketboard.biz>
2011-04-26 13:42:00 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '42866a22f306db64efda994c2f0c6085338cf895a765da207f393b4e985228ff7d3e3b29869a5e9f15ae7024750434d7b1ce97e17372cdb4cb69dc8d49764927'
