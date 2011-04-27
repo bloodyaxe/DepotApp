b6d8014a8f8a8536ed9c8d74a96695fb928b2cc1
bloodyaxe <github@marketboard.biz>
2011-04-27 12:09:30 +0300

    27.04.10 - morning

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01, :message => 'Blyaaa!!!'}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with    => %r{\.(gif|jpg|png)$}i,
