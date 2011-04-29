61926a5021cb9b849a8a38068a72e6b1089fc51e
bloodyaxe <github@marketboard.biz>
2011-04-29 15:38:53 +0300

    Catalog

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Depot::Application.routes.draw do
-  resources :products
-  
-  resources :fucker
+  get "store/index"
 
+  resources :products
+
+  root :to => "store#index", :as => "store"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
